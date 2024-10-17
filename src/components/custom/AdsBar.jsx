58233dc75c31b247be37aefdce49174807be346d
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-10-17 19:41:41 +0300

    Responsive ADS

--- src/components/custom/AdsBar.jsx
+++ src/components/custom/AdsBar.jsx
@@ -2,37 +2,41 @@ import React from 'react'
 import StarIcon from '@/assets/images/patterns/start_icon.svg'
 
 export const AdsBar = () => {
+    const styles = {
+        ContainerStyle: `Bungee_font flex gap-12 text-xl md:text-3xl whitespace-nowrap animate-marquee`,
+        TextStyle: `flex items-center gap-5`
+    }
     return (
-        <div className="Bungee_font flex gap-12 text-xl md:text-3xl whitespace-nowrap animate-marquee">
-            <span className="flex items-center gap-5">
+        <div className={styles.ContainerStyle}>
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 GAMING SPANNING
             </span>
-            <span className="flex items-center gap-5">
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 ACTION - PACKED
             </span>
-            <span className="flex items-center gap-5">
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 MIND - BENDING
             </span>
-            <span className="flex items-center gap-5">
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 COLLECTION OF GAMES
             </span>
-            <span className="flex items-center gap-5">
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 GAMING SPANNING
             </span>
-            <span className="flex items-center gap-5">
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 ACTION - PACKED
             </span>
-            <span className="flex items-center gap-5">
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 MIND - BENDING
             </span>
-            <span className="flex items-center gap-5">
+            <span className={styles.TextStyle}>
                 <img src={StarIcon} alt="Icon Star" />
                 COLLECTION OF GAMES
             </span>
